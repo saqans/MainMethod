3c3c546bdc7f2cbae35181cb253363089664165a
Saqib Ansari <saqans@gmail.com>
2019-07-12 01:16:49 -0400

    1st commit for MainMethod Project Lesson - comments

---
+++ academy/learnprogramming/Main.java
@@ -0,0 +1,30 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        for(int i =0; i<args.length; i++)
+        System.out.println("args[ " +i +"]=" +args[i]);
+
+        int a = sum(2, 3);
+
+        System.out.println("The sum of two integers " +a);
+
+
+    }
+
+
+    /**
+     * Calculates the sum of two integers
+     * @param a user defined
+     * @param b user defined
+     * @return the sum of two integers
+     */
+    public static int sum(int a, int b){
+                return a+b;
+    }
+}
